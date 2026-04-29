3b2d5c01c6752712cbb370cdd6deba8d5b9102b7
Razvan Botea <razvanb32@gmail.com>
2026-04-29 16:54:24 +0300

    Added Custom Handlers

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import (
 )
 
 func main() {
-    handler := http.NewServeMux()
+    mux := http.NewServeMux()
+
+    mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+    mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", http.FileServer(http.Dir("./assets"))))
+    mux.HandleFunc("/healthz", readinessHandler)
+
     server := &http.Server{
         Addr: ":8080",
-        Handler: handler,
+        Handler: mux,
     }
 
     server.ListenAndServe()
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
 }
