55d0142f5b8c7793cb696168ead7d92dc9b440be
Razvan Botea <razvanb32@gmail.com>
2026-05-02 10:36:34 +0300

    Improved Architecture

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
     "net/http"
+    "sync/atomic"
+    "fmt"
 )
+
+type apiConfig struct {
+    fileserverHits atomic.Int32
+}
 
 func main() {
     mux := http.NewServeMux()
+    apiCfg := &apiConfig{}
 
-    mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-    mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", http.FileServer(http.Dir("./assets"))))
-    mux.HandleFunc("/healthz", readinessHandler)
+    fileServerHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+    mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+    mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+    mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+    mux.HandleFunc("GET /api/healthz", readinessHandler)
 
     server := &http.Server{
         Addr: ":8080",
@@ -20,6 +29,29 @@ func main() {
 }
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+
+    noRequests := cfg.fileserverHits.Load()
+    w.Write([]byte(fmt.Sprintf("<html> <body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", noRequests)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+    cfg.fileserverHits.Store(0)
+
     w.Header().Set("Content-Type", "text/plain; charset=utf-8")
     w.WriteHeader(http.StatusOK)
     w.Write([]byte("OK"))
